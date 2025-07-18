cf7aab661ace718ab091405126f9e101d5ea31ff
gebu8f <postmaster@gebu8f.com>
2025-07-18 21:32:03 +0800

    Update fw.sh

--- fw.sh
+++ fw.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ "$(id -u)" -ne 0 ]; then
+  echo "此腳本需要root權限運行" 
+  if command -v sudo >/dev/null 2>&1; then
+    exec sudo "$0" "$@"
+  else
+    echo "無sudo指令"
+    exit 1
+  fi
+fi
+
 
 # 定義顏色
 GREEN="\033[1;32m"
